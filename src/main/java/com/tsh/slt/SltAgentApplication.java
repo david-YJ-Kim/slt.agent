d15887270199ac13cec26a17b300c48a68b1608f
DavidKim <115195328+david-YJ-Kim@users.noreply.github.com>
2024-09-22 14:17:11 -0400

    Initialize 프로젝트

--- src/main/java/com/tsh/slt/SltAgentApplication.java
+++ src/main/java/com/tsh/slt/SltAgentApplication.java
@@ -2,14 +2,13 @@ package com.tsh.slt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 //@EnableEurekaClient
 @SpringBootApplication
-public class SltServerApplication {
+public class SltAgentApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SltServerApplication.class, args);
+        SpringApplication.run(SltAgentApplication.class, args);
     }
 
 }
